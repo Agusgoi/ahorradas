525a62efca967998b12a37c8c59ba43a9e6d95be
Agusgoi <agus.goitea@live.com.ar>
2022-12-12 00:37:32 -0300

    table corrected - btnAdd

--- main.js
+++ main.js
@@ -13,49 +13,45 @@ let $balanceSection = $ ('#balance-section');
 let $opSection = $ ('#op-section');
 let $divImgOp = $ ('#div-img-op');
 let $opForm = $ ('#op-form');
-
-
+let $opTable = $ ('#op-table');
+let $tableBody = $ ('#table-body');
 
 
 let $description = $ ('#description');
 let $amountInput = $ ('#amount-input');
 let $typeSelect = $ ('#type-select');
 
-console.log ($opForm)
 
 $btnNewOp.addEventListener ("click", (event)=>{
     $balanceSection.classList.add ("is-hidden");
     $opSection.classList.remove ("is-hidden");
-
- })
+})
 
  $btnCancel.addEventListener ("click", (event)=>{
     $balanceSection.classList.remove ("is-hidden");
     $opSection.classList.add ("is-hidden");
- })
+})
 
-/* $btnAdd.addEventListener ("click", (event)=>{
+   $btnAdd.addEventListener ("click", (event)=>{
    $opSection.classList.add ("is-hidden");
    $balanceSection.classList.remove ("is-hidden");
-   $divImgOp.classList.remove ("is-hidden");
-}) */
+   $divImgOp.classList.add ("is-hidden");
+   $opTable.classList.remove ("is-hidden");
 
-
+}) 
 
  $opForm.addEventListener ('submit', (event)=>{
    event.preventDefault ();
-   console.log (event.target.description.value)
-   console.log (event.target.amount.value)
-   console.log (event.target.type.value)
-   console.log (event.target.category.value)
-   console.log (event.target.date.value)
-
-   let descriptionValue = event.target.description.value
-   
-
-   
+   $tableBody.innerHTML += `<td>${event.target.description.value}</td>`;
+   $tableBody.innerHTML += `<td>${event.target.category.value}</td>`;
+   $tableBody.innerHTML += `<td>$ ${event.target.amount.value}</td>`;
+   $tableBody.innerHTML += `<td>${event.target.date.value}</td>`;
+   $tableBody.innerHTML += `<td><a href="">Editar</a>
+   <a href="">Eliminar</a></td>`;   
  })
+
+ 
    
-  // textInf.innerText = event.target.value;
+ 
 
 
